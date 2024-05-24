bb036809472ce011722a6ea1123c8874087ab935
Simeon Wong <simeon_wong@smartone.com>
2024-05-24 10:42:52 +0800

    init file

---
+++ api/webhook.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+module.exports = async (request, response) => {
+	try {
+		const bot = new TelegramBot(process.env.TG_BOT_TOKEN);
+
+		const { body } = request;
+		if (body.message) {
+			const {
+				chat: { id },
+				text,
+			} = body.message;
+
+			const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+
+			await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
+		}
+	} catch (error) {
+		console.error('Error sending message');
+		console.log(error.toString());
+	}
+	response.send('OK');
+};
